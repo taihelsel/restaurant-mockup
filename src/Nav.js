d3bfb212e3dcde816364e3fc031226ed4c798a68
taihelsel <tai.bayard@gmail.com>
2018-03-09 15:53:24 -0800

    can select between nav items

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 class Nav extends Component {
+  changeNav = (e) =>{
+    document.getElementsByClassName("selected-nav-item")[0].classList.remove("selected-nav-item");
+    e.currentTarget.classList.add("selected-nav-item");
+  }
   render() {
     return (
       <ul id="Nav">
         <li className="nav-logo"><img src={require("./images/logo.png")} alt="Gorgai Thai Logo"/></li>
-        <li id="nav-home" className="nav-item selected-nav-item"><a href="#Home">Home</a></li>
-        <li id="nav-menu" className="nav-item"><a href="#Menu">Menu</a></li>
-        <li id="nav-about" className="nav-item"><a href="#About">About</a></li>
+        <li onClick={this.changeNav} id="nav-home" className="nav-item selected-nav-item"><a href="#Home">Home</a></li>
+        <li onClick={this.changeNav} id="nav-menu" className="nav-item"><a href="#Menu">Menu</a></li>
+        <li onClick={this.changeNav} id="nav-about" className="nav-item"><a href="#About">About</a></li>
       </ul>
     );
   }
