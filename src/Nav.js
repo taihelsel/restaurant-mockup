166d5c15a529b97a4d0d97ef5d5c533a4f19237d
taihelsel <tai.helsel@gmail.com>
2018-03-09 15:24:08 -0800

    added logo to nav. Added selected item

--- src/Nav.js
+++ src/Nav.js
@@ -4,7 +4,8 @@ class Nav extends Component {
   render() {
     return (
       <ul id="Nav">
-        <li id="nav-home" className="nav-item"><a href="#Home">Home</a></li>
+        <li className="nav-logo"><img src={require("./images/logo.png")} alt="Gorgai Thai Logo"/></li>
+        <li id="nav-home" className="nav-item selected-nav-item"><a href="#Home">Home</a></li>
         <li id="nav-menu" className="nav-item"><a href="#Menu">Menu</a></li>
         <li id="nav-about" className="nav-item"><a href="#About">About</a></li>
       </ul>
