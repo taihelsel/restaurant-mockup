5dc8b9e1e2487ac163c3e5b9628d6e2be8ffdf51
taihelsel <tai.bayard@gmail.com>
2018-03-14 16:05:04 -0700

    added owner img and quote

--- src/About Page/About.js
+++ src/About Page/About.js
@@ -10,8 +10,8 @@ class About extends Component {
       <div id="About">
         <Slideshow />
         <div className="owner-quote-wrapper">
-          <img className="owner-image" />
-          
+          <p className="owner-quote">"We proudly serve traditional Thai street food. Fresh, authentic, and delicious cuisine with bold flavors. Gor Gai Thai."</p>
+          <img className="owner-image" src="https://static1.squarespace.com/static/54d2af1ee4b0526a6669799d/t/579923b120099eba44e2921b/1469654082698/" alt="Owner of gorgaithai headshot" />
         </div>
       </div>
     );
